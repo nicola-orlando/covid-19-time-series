a02cc338d98131bcaf3b0e968e5986de344fa2d5
Nicola Orlando <orlando@nicolas-air.home>
2020-05-04 13:34:40 +0200

    updating covid_time_series.py

--- covid_time_series.py
+++ covid_time_series.py
@@ -27,18 +27,20 @@ def group_and_sum(dataframe,first_feature,second_feature,manipulated_data_first,
     return grouped_data
 
 # Get list of countries to analyse
-def list_of_countries(dataframe,test_run=True):
-    print('\n----> Obtaining list of countries')
+def list_of_countries(dataframe,test_run=True,print_verbose=False):
+    if print_verbose:
+        print('\n----> Obtaining list of countries')
     countries=[]
     if test_run:
-            countries.append('Italy')
+            countries.append('Sweden')
     else: 
         countries=dataframe.Country_Region.unique()
     return countries 
 
 # Plot input data 
-def plot_series(dataframe,country):
-    print('\n----> Plotting input series')
+def plot_series(dataframe,country,print_verbose=False):
+    if print_verbose:
+        print('\n----> Plotting input series')
     features=['ConfirmedCases','Fatalities']
     dataframe=dataframe[dataframe.Country_Region == country]    
     dataframe=dataframe[dataframe.ConfirmedCases != 0]    
@@ -69,8 +71,9 @@ def plot_series(dataframe,country):
     plt.close()
 
 # Cleanup, rearrange and select a given country in the dataset
-def prepare_dataset_for_training(dataframe,country,feature_to_predict_first,feature_to_predict_second,feature_to_predict_name_first,feature_to_predict_name_second):
-    print('\n----> Minimal manipulation on training data')
+def prepare_dataset_for_training(dataframe,country,feature_to_predict_first,feature_to_predict_second,feature_to_predict_name_first,feature_to_predict_name_second,print_verbose=False):
+    if print_verbose:
+        print('\n----> Minimal manipulation on training data')
     dataframe=dataframe[dataframe.Country_Region == country]    
     # Remove entries with no data
     dataframe=dataframe[feature_to_predict_first != 0]    
@@ -78,19 +81,22 @@ def prepare_dataset_for_training(dataframe,country,feature_to_predict_first,feat
     return dataframe 
 
 # Pickup the relevant country only
-def prepare_dataset_for_testing(dataframe,country):
-    print('\n----> Pickup the country from the testing data corresponding to the country selected in the training data')
+def prepare_dataset_for_testing(dataframe,country,print_verbose=False):
+    if print_verbose:
+        print('\n----> Pickup the country from the testing data corresponding to the country selected in the training data')
     dataframe=dataframe[dataframe.Country_Region == country]    
     return dataframe 
 
 # Same idea as illustrated in this post https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
-def transpose_the_dataset(dataframe,feature,number_of_shifts=0):
+def transpose_the_dataset(dataframe,feature,number_of_shifts=0,print_verbose=False):
     # Gets the full lenght of the dataset 
-    print('\n----> Shifting the dataset to convert it into a useful format for a classification problem')
+    if print_verbose:
+        print('\n----> Shifting the dataset to convert it into a useful format for a classification problem')
     if number_of_shifts == 0: 
         number_of_shifts = len(dataframe[feature])
-    print('Transposing with '+str(number_of_shifts)+' data steps')
-    print(dataframe.head())
+    if print_verbose:
+        print('Transposing with '+str(number_of_shifts)+' data steps')
+        print(dataframe.head())
     for index in range(1,number_of_shifts+1): 
         name_feature_added=feature+'_'+str(index)
         dataframe[name_feature_added]=dataframe[feature].shift(index)
@@ -105,7 +111,8 @@ def save_model(name,model):
 
 # Define and train the model, return the score
 def train_model(X_train,y_train,output_modelname,print_verbose=False):
-    print("\n----> Training a Bayesian Ridge model")
+    if print_verbose:
+        print("\n----> Training a Bayesian Ridge model")
     bayesian_model = linear_model.BayesianRidge()
     bayesian_model.fit(X_train,y_train)
     score_predictions_train = bayesian_model.predict(X_train)
@@ -116,8 +123,9 @@ def train_model(X_train,y_train,output_modelname,print_verbose=False):
     return score_predictions_train
 
 # Prepare some validation plots for the model predictions on trained data
-def plot_model_predictions_vs_time(dataframe,base_feature,model_prediction,base_feature_title): 
-    print('\n----> Plotting model prediction vs time')
+def plot_model_predictions_vs_time(dataframe,base_feature,model_prediction,base_feature_title,print_verbose=False): 
+    if print_verbose:
+        print('\n----> Plotting model prediction vs time')
     x_len=len(dataframe[base_feature].to_numpy())*0.19
     dataframe['Data-Over-Predicted']=dataframe[base_feature]/dataframe[model_prediction]
     figure(figsize=(x_len, 4.8))
@@ -144,8 +152,9 @@ def plot_model_predictions_vs_time(dataframe,base_feature,model_prediction,base_
     plt.close()
 
 #  Prepare some validation plots for the model predictions on trained data (2D plots)
-def plot_data_vs_predicted(dataframe,base_feature,model_prediction,base_feature_title): 
-    print('\n----> Plotting data vs prediction')
+def plot_data_vs_predicted(dataframe,base_feature,model_prediction,base_feature_title,print_verbose=False): 
+    if print_verbose:
+        print('\n----> Plotting data vs prediction')
     plt.plot(dataframe[base_feature], dataframe[model_prediction], marker='', linewidth=1, alpha=0.9, label=base_feature_title,)
     plt.grid(True)
     plt.title(base_feature_title+' compared to model predictions')
@@ -157,18 +166,20 @@ def plot_data_vs_predicted(dataframe,base_feature,model_prediction,base_feature_
     plt.close()
 
 # Add score on days which overalp in training and testing dataset
-def add_score_on_overlapping_days(dataframe_base,dataframe_test):
-    print('\n----> Add prediction on overlapping testing/training datasets')
+def add_score_on_overlapping_days(dataframe_base,dataframe_test,print_verbose=False):
+    if print_verbose:
+        print('\n----> Add prediction on overlapping testing/training datasets')
     dataframe_merged = pd.merge(dataframe_base,dataframe_test,on='Date',how='left')
     # Ensure to keep only the intersection of the two sets by forcing the ID of the second to be not a Nan
     dataframe_merged=dataframe_merged[dataframe_merged.ForecastId==dataframe_merged.ForecastId]
     return dataframe_merged
 
 # Obtain the first row in the testing dataset which has no prediction from the trained model, this is to be used to evaluate the model
-# Expected output structure [ConfirmedCases_1,ConfirmedCases_75]
+# Expected output structure [ConfirmedCases_1,..ConfirmedCases_75]
+# This will aslo use the last row with available prediction where the score will be used for the last available data entry ('_1' feature)
 def extract_last_row_for_testing(dataframe,number_of_shifts,print_verbose=False): 
-    print('Extract first emtpy row from testing data to be filled up with model prediction')
     if print_verbose: 
+        print('Extract first emtpy row from testing data to be filled up with model prediction')
         print('In extract_last_row_for_testing printing input data')
         print(dataframe.head())
     # Extract first row without a prediction
@@ -197,7 +208,8 @@ def extract_last_row_for_testing(dataframe,number_of_shifts,print_verbose=False)
     return row_predicted
 
 def load_and_apply_the_model(dataframe,model_name,print_verbose=False):
-    print('\n----> Loading and applying the model')
+    if print_verbose: 
+        print('\n----> Loading and applying the model')
     loaded_model = pickle.load(open(model_name+'.pkl', 'rb'))
     score_prediction = loaded_model.predict(dataframe)
     if print_verbose:
@@ -205,8 +217,8 @@ def load_and_apply_the_model(dataframe,model_name,print_verbose=False):
     return score_prediction
 
 def replace_first_row(dataframe_bottom,row,print_verbose=False): 
-    print('\n----> Replacing now the first row of the final testing dataset')
     if print_verbose:
+        print('\n----> Replacing now the first row of the final testing dataset')
         print(dataframe_bottom.head())
         print(row.head())
     row_array = row.to_numpy()
@@ -215,6 +227,14 @@ def replace_first_row(dataframe_bottom,row,print_verbose=False):
         print('After conversion')
         print(dataframe_bottom.head())
     return dataframe_bottom
+
+def add_feature(dataframe_base,series,title,print_verbose=False): 
+    array=series.to_numpy()
+    dataframe_base['ConfirmedCases']=array
+    if print_verbose:
+        print(array)
+        print(dataframe_base.head())
+    return dataframe_base
 
 # Header will look like this 
 # Id Province_State Country_Region Date  ConfirmedCases  Fatalities
@@ -290,24 +310,43 @@ for country in countries:
     print('\nPrint dataframe_merged (testing with scores appened) after cleanup')
     print(dataframe_merged.head())
 
-    dataframe_to_be_evaluated = extract_last_row_for_testing(dataframe_merged,number_of_shifts)
-    print('\nHere printing the one row dataframe holding the data to be used to obtain the prediction')
-    print(dataframe_to_be_evaluated)
-    
-    score_to_be_saved = load_and_apply_the_model(dataframe_to_be_evaluated,'logreg')
-    dataframe_to_be_evaluated['score_predictions_bayes']=score_to_be_saved
-    print('\nOne row dataframe score appended')   
-    print(dataframe_to_be_evaluated.head())
-    
-    #dataframe_merged = dataframe_merged.mask(dataframe_merged.score_predictions_bayes != dataframe_merged.score_predictions_bayes, dataframe_to_be_evaluated)
-    print(dataframe_merged.head(15))
 
+    # Split dataframe into two parts, one which has already the score saved (first), the other which doesn't (second)
     dataframe_merged_first = dataframe_merged[dataframe_merged['score_predictions_bayes'] == dataframe_merged['score_predictions_bayes']]
-
     dataframe_merged_second = dataframe_merged[dataframe_merged['score_predictions_bayes'] != dataframe_merged['score_predictions_bayes']]
-        
-    dataframe_merged_second_replaced = replace_first_row(dataframe_merged_second,dataframe_to_be_evaluated)
+    
+    # To be updated in the while loop below
+    dataframe_concatenated = pd.concat([dataframe_merged_first,dataframe_merged_second])
 
-    # Stick the data 
-    dataframe_concatenated = pd.concat([dataframe_merged_first,dataframe_merged_second_replaced])
-    print(dataframe_concatenated.head(15))
+    # Look to fill up the scores for the second dataframe andeventually attach it back to the first
+    index = 0
+    while dataframe_merged_second.score_predictions_bayes.size !=  1: 
+        print('\nRemaining days to predict '+str(dataframe_merged_second.score_predictions_bayes.size))
+        index = index+1
+        if index ==1:
+            dataframe_to_be_evaluated = extract_last_row_for_testing(dataframe_merged,number_of_shifts)
+        else: 
+            #print(dataframe_merged_second.head())
+            dataframe_to_be_evaluated = extract_last_row_for_testing(dataframe_merged_second,number_of_shifts)
+
+        # Redefine second dataset taking out the entries with score already filled up
+        dataframe_merged_second=dataframe_merged_second[dataframe_merged_second['score_predictions_bayes'] != dataframe_merged_second['score_predictions_bayes']]
+        score_to_be_saved = load_and_apply_the_model(dataframe_to_be_evaluated,'logreg')
+        dataframe_to_be_evaluated['score_predictions_bayes']=score_to_be_saved
+
+        dataframe_merged_second = replace_first_row(dataframe_merged_second,dataframe_to_be_evaluated)
+        
+        # Glue the data 
+        dataframe_concatenated = pd.concat([dataframe_concatenated,dataframe_merged_second])
+    
+    # Now cleanup the dataframe with the predictions (remove nans)
+    dataframe_concatenated=dataframe_concatenated[dataframe_concatenated.score_predictions_bayes == dataframe_concatenated.score_predictions_bayes]
+    print(dataframe_concatenated.head())
+    df_for_testing_copy = df_for_testing.copy()
+    print(df_for_testing_copy.head())
+
+    # Finally add predictions to the testing dataframe and save it as csv
+    final_output=add_feature(df_for_testing_copy,dataframe_concatenated.score_predictions_bayes,'test') 
+
+    title_output='testing_confirmed_cases_'+country+'.csv'
+    final_output.to_csv(title_output, index=False) 
